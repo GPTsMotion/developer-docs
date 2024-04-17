27216bfb25f83f758d4d00c444f76e0f7b566e9e
river <laolei@forkway.cn>
2024-04-17 23:01:13 +0800

    Update fonts and colors in script.js and mint.json

--- script.js
+++ script.js
@@ -6,9 +6,3 @@ var link = document.createElement('link');
 link.href = 'https://fonts.googleapis.com/css2?family=Urbanist:wght@400;500;600;800&display=swap';
 link.rel = 'stylesheet';
 document.head.appendChild(link);
-
-{/* <link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro:wght@400;600;700&display=swap" rel="stylesheet"> */}
-var link2 = document.createElement('link');
-link2.href = 'https://fonts.googleapis.com/css2?family=Merriweather:wght@400;600;700&display=swap';
-link2.rel = 'stylesheet';
-document.head.appendChild(link2);
