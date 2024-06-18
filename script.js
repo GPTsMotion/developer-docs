d1a789bda877294a50f9852a80e49be75f271e3b
river <laolei@forkway.cn>
2024-06-18 16:56:18 +0800

    chore: add meta

--- script.js
+++ script.js
@@ -6,3 +6,4 @@ var link = document.createElement('link');
 link.href = 'https://fonts.googleapis.com/css2?family=Urbanist:wght@400;500;600;800&display=swap';
 link.rel = 'stylesheet';
 document.head.appendChild(link);
+
